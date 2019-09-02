47a328629b81d000dfef2327da0acc9db2cf64ec
andres-martin <andresmartin266@gmail.com>
2019-09-01 23:04:24 -0500

    change state value in App component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    }
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.increaseCounter}>Incrementa</button>
       </div>
     );
   }
+
+
+  increaseCounter = () => {
+    let counter = this.state.counter
+    counter++
+    this.setState({
+      counter: counter
+    });
+  }
+
+
 }
 
 export default App;
